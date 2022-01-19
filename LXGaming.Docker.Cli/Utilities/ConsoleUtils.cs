b08c7663abde87126605619747a3b00e5fc91aa6
Alex Thomson <LXGaming@users.noreply.github.com>
2022-01-19 15:33:07 +1300

    Change default confirmation value to true

--- LXGaming.Docker.Cli/Utilities/ConsoleUtils.cs
+++ LXGaming.Docker.Cli/Utilities/ConsoleUtils.cs
@@ -6,7 +6,7 @@ public static class ConsoleUtils {
 
     public static bool Confirm(string prompt) {
         return AnsiConsole.Prompt(new ConfirmationPrompt(prompt) {
-            DefaultValue = false,
+            DefaultValue = true,
             ShowChoices = false,
             ShowDefaultValue = false
         });
