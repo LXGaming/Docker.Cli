d609cc954b4b5c07ed37dea0c7a52273770e9ac1
Alex Thomson <LXGaming@users.noreply.github.com>
2022-07-24 11:28:50 +1200

    Remove trailing spaces

--- LXGaming.Docker.Cli/Utilities/Constants.cs
+++ LXGaming.Docker.Cli/Utilities/Constants.cs
@@ -1,13 +1,13 @@
 ﻿using System.Reflection;
 
-namespace LXGaming.Docker.Cli.Utilities; 
+namespace LXGaming.Docker.Cli.Utilities;
 
 public static class Constants {
 
     public static class Application {
 
         public const string Name = "Docker.Cli";
-        
+
         public static readonly string Version = Toolbox.GetAssemblyVersion(Assembly.GetExecutingAssembly());
     }
 }
--- LXGaming.Docker.Cli/Utilities/Toolbox.cs
+++ LXGaming.Docker.Cli/Utilities/Toolbox.cs
@@ -1,9 +1,9 @@
 ﻿using System.Reflection;
 
-namespace LXGaming.Docker.Cli.Utilities; 
+namespace LXGaming.Docker.Cli.Utilities;
 
 public static class Toolbox {
-    
+
     public static string GetAssemblyVersion(Assembly assembly) {
         return (assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion
                 ?? assembly.GetCustomAttribute<AssemblyVersionAttribute>()?.Version
