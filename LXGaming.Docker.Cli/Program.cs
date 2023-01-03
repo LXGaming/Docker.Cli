76ec4de9af1cf2fed2da4a87ab48dbf9ca451ac1
Alex Thomson <LXGaming@users.noreply.github.com>
2023-01-03 16:24:50 +1300

    Convert Program to Top-level statement

--- LXGaming.Docker.Cli/Program.cs
+++ LXGaming.Docker.Cli/Program.cs
@@ -4,34 +4,22 @@ using LXGaming.Docker.Cli.Utilities;
 using Spectre.Console;
 using Spectre.Console.Cli;
 
-namespace LXGaming.Docker.Cli;
+var app = new CommandApp();
+app.Configure(config => {
+    config.SetApplicationName(Constants.Application.Name);
+    config.SetApplicationVersion(Constants.Application.Version);
+    config.PropagateExceptions();
 
-public static class Program {
+    config.AddCommand<ComposeCommand>("compose");
+    config.AddCommand<UpdateCommand>("update");
+});
 
-    public static int Main(string[] args) {
-        AppDomain.CurrentDomain.ProcessExit += (_, _) => Shutdown();
-        Console.CancelKeyPress += (_, _) => Shutdown();
-
-        var app = new CommandApp();
-        app.Configure(config => {
-            config.SetApplicationName(Constants.Application.Name);
-            config.SetApplicationVersion(Constants.Application.Version);
-            config.PropagateExceptions();
-
-            config.AddCommand<ComposeCommand>("compose");
-            config.AddCommand<UpdateCommand>("update");
-        });
-
-        try {
-            return app.Run(args);
-        } catch (Exception ex) {
-            AnsiConsole.WriteException(ex);
-            return 1;
-        }
-    }
-
-    private static void Shutdown() {
-        AnsiConsole.Cursor.Show();
-        AnsiConsole.Reset();
-    }
+try {
+    return app.Run(args);
+} catch (Exception ex) {
+    AnsiConsole.WriteException(ex);
+    return 1;
+} finally {
+    AnsiConsole.Cursor.Show();
+    AnsiConsole.Reset();
 }
